6e742327e9628d50e9207402a5553a252c8e77a1
ezio84 <brabus84@gmail.com>
2016-02-25 13:02:00 +0100

    CM navbar squashed revert p2: I love the smell of napalm in the morning.
    Revert "(2/2) Settings: allow to configure the long press back kill delay"
    
    This reverts commit a1183f113e5aae96bb3b89f13c8ea93812f11e95.
    
    Revert a part of "move utils to common folder"
    
    Revert "Settings : Add back navigation bar customization."
    
    This reverts commit f3eff731e18d47a1409e71dcedd538fc338c9cf3.
    
    Revert "Settings: add navigation bar arrow keys toggle"
    
    This reverts commit 454b4117b78dbe346fa0ac8f4a0c512dfade0378.
    
    Revert "Double tap to sleep on navigation bar [2/2]"
    
    This reverts commit 859bece24589b617d33ce92a4c5f305fe612bdc0.
    
    Revert "Navbar IME selector toggle"
    
    This reverts commit 2a0f645243239f367ef16c307bb0e4dbddf98df1.
    
    Revert "Settings: Navigation bar options (2/2)"
    
    This reverts commit 970ca2c730f51923090d8b52a0736efe4e7a65b9.
    
    Revert "longpress recents last app toggle"
    
    This reverts commit 6b30894716a21bcbd1884ec4f8481fe07c452de6.
    
    Revert "Settings: Forward port long-press-back to kill app (1/2)"
    
    This reverts commit 8e27e9b135ab2e65b0aa405cc22abb87b90b0a29.

--- src/com/android/purenexussettings/NavbarFragment.java
+++ src/com/android/purenexussettings/NavbarFragment.java
@@ -13,90 +13,28 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.android.purenexussettings;
 
-import android.content.ContentResolver;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.preference.Preference;
-import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
-import android.provider.Settings;
+import android.support.annotation.NonNull;
 
-import com.android.purenexussettings.preferences.SeekBarPreference;
-
-public class NavbarFragment extends PreferenceFragment
-            implements OnPreferenceChangeListener  {
-
-    private static final String CATEGORY_NAVBAR = "navigation_bar";
-
-    private static final String NAVDIMEN = "navbar_dimen_frag";
-    private static final String NAVBUTTON = "navbar_button_frag";
-    private static final String LONG_PRESS_KILL_DELAY = "long_press_kill_delay";
-
-    private Preference mNavDimen;
-    private Preference mNavButton;
-    private SeekBarPreference mLongpressKillDelay;
-
+public class NavbarFragment extends PreferenceFragment {
     public NavbarFragment(){}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.navbar_fragment);
-
-        final PreferenceScreen prefScreen = getPreferenceScreen();
-        ContentResolver resolver = getActivity().getContentResolver();
-
-        final PreferenceCategory navbarCategory =
-                (PreferenceCategory) prefScreen.findPreference(CATEGORY_NAVBAR);
-
-        mNavDimen = (Preference)findPreference(NAVDIMEN);
-        mNavButton = (Preference)findPreference(NAVBUTTON);
-
-        mLongpressKillDelay = (SeekBarPreference) findPreference(LONG_PRESS_KILL_DELAY);
-        int killconf = Settings.System.getInt(resolver,
-                Settings.System.LONG_PRESS_KILL_DELAY, 1000);
-        mLongpressKillDelay.setValue(killconf);
-        mLongpressKillDelay.setOnPreferenceChangeListener(this);
-
-        // Enable or disable NavbarImeSwitcher based on boolean: config_show_cmIMESwitcher
-        boolean showCmImeSwitcher = getResources().getBoolean(
-                com.android.internal.R.bool.config_show_cmIMESwitcher);
-        if (!showCmImeSwitcher) {
-            Preference pref = findPreference(Settings.System.STATUS_BAR_IME_SWITCHER);
-            if (pref != null) {
-                navbarCategory.removePreference(pref);
-            }
-        }
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mLongpressKillDelay) {
-            int killconf = (Integer) newValue;
-            Settings.System.putInt(resolver,
-                    Settings.System.LONG_PRESS_KILL_DELAY, killconf);
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public boolean onPreferenceTreeClick(PreferenceScreen prefScreen, Preference pref) {
-        if (pref == mNavDimen) {
-            ((TinkerActivity)getActivity()).displaySubFrag(getString(R.string.navbardimenfrag_title));
-
-            return true;
-        }
-        if (pref == mNavButton) {
-            ((TinkerActivity)getActivity()).displaySubFrag(getString(R.string.navbarbuttonfrag_title));
-
-            return true;
-        }
+    public boolean onPreferenceTreeClick(PreferenceScreen prefScreen, @NonNull Preference pref) {
         return false;
     }
 }
